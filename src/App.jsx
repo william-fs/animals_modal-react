eb291cd90dc9dc5edb13eed7d5f4cb35019e279c
William Ferreira <wfs19@live.com>
2023-12-21 13:13:34 -0300

    Finalizado as requisições de todos animais, mudando mantendo props nos componentes reaproveitando para gerar pos cards de acordo com o animal escolhido. Também ajustado o responsivo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,130 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import BtnCall from "./components/BtnCall/BtnCall";
 import Modal from "./components/Modal/Modal";
-import fetchCachorro from "./fetchApi/fetchCachorro";
 import Loading from "./components/Loading/Loading";
 
 
 function App() {
 
   const [statusModal, setStatusModal] = useState(false);
-  const [imagemCachorro, setImagemCachorro] = useState("");
   const [carregando, setCarregando] = useState(<Loading />);
-  const [imagemGato, setImagemGato] = useState("");
-  const [imagemPato, setImagemPato] = useState("");
-  const [imagemRaposa, setImagemRaposa] = useState("");
+  const [imagemEscolhida, setImagemEscolhida] = useState("");
   const [animalEscolhido, setAnimalEscolhido] = useState("");
 
   // Fazendo requisição de API de acordo com o animal escolhido 
+
+  // Gato
+  async function callCat() {
+    if(imagemEscolhida == "") {
+      setStatusModal(true);
+      setAnimalEscolhido("Gato");
+      await fetch("https://api.thecatapi.com/v1/images/search")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setImagemEscolhida(data[0].url);
+        })
+
+        setCarregando("");
+    }
+  }
+
+  // Cachorro 
   async function callDog() {
-    if(imagemCachorro == "") {
-      await setStatusModal(true);
-      await setAnimalEscolhido("Cachorro");
+    if(imagemEscolhida == "") {
+      setStatusModal(true);
+      setAnimalEscolhido("Cachorro");
       await fetch("https://dog.ceo/api/breeds/image/random")
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          setImagemCachorro(data.message);
+          setImagemEscolhida(data.message);
         })
 
         setCarregando("");
     }
-    
   }
 
-  // Escolhendo qual animal chamar 
-  function callCat() {
-    setStatusModal(true);
-    setAnimalEscolhido("Gato");
+  // Raposa
+  async function callFox() {
+    if(imagemEscolhida == "") {
+      setStatusModal(true);
+      setAnimalEscolhido("Raposa");
+      await fetch("https://randomfox.ca/floof/")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setImagemEscolhida(data.image);
+        })
+
+        setCarregando("");
+    }
   }
 
   function callDuck() {
-    setStatusModal(true);
-    setAnimalEscolhido("Pato");
+
   }
 
-  function callFox() {
-    setStatusModal(true);
-    setAnimalEscolhido("Raposa");
-  }
 
   // Solicitando mais um no botão dentro do modal: 
   async function maisUm() {
+
+    // Gato 
+    if(animalEscolhido == "Gato") {
+      setCarregando(<Loading />);
+      setImagemEscolhida("");
+      await fetch("https://api.thecatapi.com/v1/images/search")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setImagemEscolhida(data);
+          setImagemEscolhida(data[0].url);
+        })
+
+        setCarregando("");
+    }
     
+    // Cachorro 
     if(animalEscolhido == "Cachorro") {
       setCarregando(<Loading />);
-      setImagemCachorro("");
+      setImagemEscolhida("");
       await fetch("https://dog.ceo/api/breeds/image/random")
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          setImagemCachorro(data.message);
-          
+          setImagemEscolhida(data.message);
         })
 
         setCarregando("");
     }
+
+    // Raposa
+    if(animalEscolhido == "Raposa") {
+      setCarregando(<Loading />);
+      setImagemEscolhida("");
+      await fetch("https://randomfox.ca/floof/")
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          setImagemEscolhida(data.image);
+        })
+
+        setCarregando("");
+    }
+
   }
 
   // Fechar modal 
   function closeModal(event) {
     if(event.target.className === 'closeX' || event.target.className === 'modal') {
       setStatusModal(false);
-      setImagemCachorro("");
+      setImagemEscolhida("");
       setCarregando(<Loading />)
     }
   }
@@ -81,15 +133,12 @@ function App() {
     <div className="app">
       <h1>Modal de Animais</h1>
       <div className="boxBtn">
-        <BtnCall btnColor={"btnDog"} btnText={"Cachorro"} call={callDog} imgAnimal={imagemCachorro} />
-        <BtnCall btnColor={"btnCat"} btnText={"Gato"} call={callCat} imgAnimal={imagemGato} />
-      </div>
-      <div className="boxBtn">
-        <BtnCall btnColor={"btnDuck"} btnText={"Pato"} call={callDuck} imgAnimal={imagemPato} />
-        <BtnCall btnColor={"btnFox"} btnText={"Raposa"} call={callFox} imgAnimal={imagemRaposa} />
+        <BtnCall btnColor={"btnCat"} btnText={"Gato"} call={callCat} imgAnimal={imagemEscolhida} />
+        <BtnCall btnColor={"btnDog"} btnText={"Cachorro"} call={callDog} imgAnimal={imagemEscolhida} />
+        <BtnCall btnColor={"btnFox"} btnText={"Raposa"} call={callFox} imgAnimal={imagemEscolhida} />
       </div>
 
-      <Modal status={statusModal} animal={animalEscolhido} closeMX={closeModal} closeME={closeModal} imgAnimal={imagemCachorro} load={carregando} outro={maisUm} />
+      <Modal status={statusModal} animal={animalEscolhido} closeMX={closeModal} closeME={closeModal} imgAnimal={imagemEscolhida} load={carregando} outro={maisUm} />
 
     </div>
   )
--- src/fetchApi/fetchCachorro.js
+++
@@ -1,7 +0,0 @@
-async function fetchCachorro() {
-    const response = await fetch("https://dog.ceo/api/breeds/image/random");
-    const data = await response.json();
-    data.message;
-}
-
-export default fetchCachorro;
